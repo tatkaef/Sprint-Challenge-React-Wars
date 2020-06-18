ced0ae96e7a0feaf60784e674dff630af6959850
Tatiana Efimova <tatka_ef@mail.ru>
2020-06-18 02:22:08 -0700

    finished

--- starwars/src/components/Header.js
+++ starwars/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import Other from './Other';
+import Starship from './Starship';
+
 
 function axiosCall (cb) {
     axios.get("https://swapi.dev/api/people/")
@@ -17,18 +18,39 @@ export default function Header() {
     }
     console.log("call",call);
 
+    const mStyle = (color) => {
+        return {
+            header: {
+                display: `flex`,
+                "align-items" : `center`, 
+                "flex-direction" : `column`,              
+            },
+            info: {
+                border: `2px solid black`,
+                "border-radius": `35px`,
+                width: `80%`,
+                margin: `1%`,
+            },
+            div: {               
+                color,
+            },            
+        }
+    }
+
+
     return (
-        <div className="header">         
+        <div className="header" style={mStyle().header}>         
         {call.map( (obj) => {
           return(
-            <div className='info'><h1 className='name'>{obj.name}</h1>
-            <h2 className='birth_year'>Birth Year: {obj.birth_year}</h2> 
-            <h2 className='gender'>Gender: {obj.gender}</h2>
-            <h2 className='homeworld'>Homeword: {obj.homeworld}</h2>
-            <button className='moreInfo'>More info </button>
-            <div className='films'> Film: {obj.films} </div> 
+            <div className='info' style={mStyle().info}>
+            <h1 className='name' style={{color:`red`}}>{obj.name}</h1>
+            <h2 className='birth_year'><span style={mStyle(`blue`).div}>Birth Year:</span> {obj.birth_year}</h2> 
+            <h2 className='gender'><span style={mStyle(`blue`).div}>Gender:</span>  {obj.gender}</h2>
+            <h2 className='homeworld'><span style={mStyle(`blue`).div}>Homeword:</span>  {obj.homeworld}</h2>
+            {/* <button className='moreInfo'>More info </button> */}
+            <div className='films'> Film: <span style={mStyle(`orange`).div}>{obj.films}</span> </div> 
             <h2 className='species'> Species: {obj.species}</h2>
-            <h2 className='starships'>Starships: {obj.tarships}</h2>
+            <Starship>Starships: {obj.starships[0]} </Starship>
             <h2 className='vehicles'>Vehicles: {obj.vehicles}</h2>
           </div>)  })}
         </div>   
---
+++ starwars/src/components/Starship.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export default styled.h2`
+    color: midnightblue;
+    font-style: italic;
+    font-family: "Lucida Console", Courier, monospace;
+    text-shadow: 2px 1px tomato;
+`
+
+// same way:
+// const Starship = styled.h2`
+// color: midnightblue;
+// font-style: italic;
+// font-family: "Lucida Console", Courier, monospace;
+//  text-shadow: 2px 1px tomato;
+// `
+// export default Starship;
